3ba555661a78de8858cdeaf594262948fafc2af1
zhangtao <tao.zhang-2@tcredit.com>
2019-06-06 15:48:37 +0800

    PWA 配置

---
+++ dist/precache-manifest.b6710a57cb2bde7f4e81d713a6efa594.js
@@ -0,0 +1,21 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4e2691cf016d66a825",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "e2b1476450aa575e3ed310b215a0ae99",
+    "url": "fonts/本墨钧宋-e2b14.min.ttf"
+  },
+  {
+    "url": "images/46d789be0ea92774ba6f49b61164cd13.jpg"
+  },
+  {
+    "revision": "334b74235cd4a8e1fe892d316502da74",
+    "url": "index.html"
+  },
+  {
+    "revision": "4b4e2691cf016d66a825",
+    "url": "main.css"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router";
-import {add} from './math.js'
+import { add } from './math.js'
 
 add(66)
 /*初始化*/
@@ -11,19 +11,33 @@ renderWithHotReload(Router);
 
 /*热更新*/
 if (module.hot) {
-  module.hot.accept("./router/index.js", () => {
-    const Router = require("./router/index.js").default;
-    renderWithHotReload(Router);
-  });
+    module.hot.accept("./router/index.js", () => {
+        const Router = require("./router/index.js").default;
+        renderWithHotReload(Router);
+    });
 }
 
 function renderWithHotReload(Router) {
-  ReactDOM.render(
-    <AppContainer>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </AppContainer>,
-    document.getElementById("app")
-  );
+    ReactDOM.render(
+        <AppContainer>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+        </AppContainer>,
+        document.getElementById("app")
+    );
+}
+
+// 判断该浏览器支不支持 serviceWorker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then(registration => {
+                console.log('service-worker registed')
+            })
+            .catch(error => {
+                console.log('service-worker registed error')
+            })
+    })
 }
